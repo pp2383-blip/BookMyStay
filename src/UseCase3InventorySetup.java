623bd9ac8318fbdae42f3bb72205662dcad85ba1
pp2383-blip <pp2383@srmist.edu.in>
2026-03-11 09:45:29 +0530

    UC3: Inventory Setup

---
+++ src/UseCase3InventorySetup.java
@@ -0,0 +1,29 @@
+import java.util.Map;
+public class UseCase3InventorySetup {
+
+    public static void main(String[] args) {
+        System.out.println("Hotel Room Inventory Status\n");
+
+        // Initialize Inventory and Rooms
+        RoomInventory inventory = new RoomInventory();
+        Map<String, Integer> availability = inventory.getRoomAvailability();
+
+        // Create Room objects to get characteristics (Price/Size)
+        Room single = new SingleRoom();
+        Room doubleRm = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        // Displaying data by pulling from the Centralized Inventory
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available Rooms: " + availability.get("Single Room") + "\n");
+
+        System.out.println("Double Room:");
+        doubleRm.displayRoomDetails();
+        System.out.println("Available Rooms: " + availability.get("Double Room") + "\n");
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available Rooms: " + availability.get("Suite Room"));
+    }
+}

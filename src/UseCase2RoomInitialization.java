7cf90de11a4f7e2e8cdf2d2f4879fec09771ea21
pp2383-blip <pp2383@srmist.edu.in>
2026-03-11 09:33:42 +0530

    UC2: Room Initialization

---
+++ src/UseCase2RoomInitialization.java
@@ -0,0 +1,27 @@
+public class UseCase2RoomInitialization {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Room Initialization\n");
+
+        Room single = new SingleRoom();
+        Room doubleRm = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvail = 5;
+        int doubleAvail = 3;
+        int suiteAvail = 2;
+
+        System.out.println(single.getRoomType());
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvail + "\n");
+
+        System.out.println(doubleRm.getRoomType());
+        doubleRm.displayRoomDetails();
+        System.out.println("Available: " + doubleAvail + "\n");
+
+        System.out.println(suite.getRoomType());
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvail);
+    }
+}
